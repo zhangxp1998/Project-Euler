8e3b2a93d79d39f1183b7a3f670d6640a2dc93f8
Kelvin Zhang <zhangxp1998@gmail.com>
2015-08-20 22:10:28 -0400

    Problem 4

--- src/P4.java
+++ src/P4.java
@@ -1,10 +1,38 @@
-
 public class P4
 {
+	public static long reverse(long n)
+	{
+		long reversed = 0;
+		while (n > 0)
+		{
+			reversed = 10 * reversed + n % 10;
+			n /= 10;
+		}
+		return reversed;
+	}
+
+	public static boolean isPalindrome(long n)
+	{
+		return n == reverse(n);
+	}
 
 	public static void main(String[] args)
 	{
-		
+		int max = 0;
+		for (int i = 999; i >= 100; i--)
+		{
+			for (int j = 999; j >= i; j--)
+			{
+				int p = i * j;
+				if (p > max && isPalindrome(p))
+				{
+					max = i * j;
+				}
+			}
+		}
+
+		System.out.println(max);
+
 	}
 
 }
