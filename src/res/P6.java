a147e0a5505c69a613ee3814657af543f02cd330
Kelvin Zhang <zhangxp1998@gmail.com>
2015-08-22 13:01:43 -0400

    add solution to problem 6

---
+++ src/res/P6.java
@@ -0,0 +1,13 @@
+package res;
+
+public class P6
+{
+
+	public static void main(String[] args)
+	{
+		//gauss formula
+		int n = 100;
+		System.out.println((int)Math.pow(((1 + n) * n >> 1), 2) - ((n << 1) + 1) * (n + 1) * n / 6);
+	}
+
+}
