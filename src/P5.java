0362bc359deb7ab4094e3f1ecbb6ad752c424bbc
Kelvin Zhang <zhangxp1998@gmail.com>
2015-08-22 12:50:35 -0400

    Add solution to problem 5

--- src/P5.java
+++ src/P5.java
@@ -1,9 +1,21 @@
-
 public class P5
 {
 
 	public static void main(String[] args)
 	{
+		int[] arr = {19, 17, 13, 11, 18, 16, 15, 14, 12};
+		outer: for(int i = 20;;i += 20)
+		{
+			for(int j = 0; j < arr.length; j ++)
+			{
+				if(i % arr[j] != 0)
+				{
+					continue outer;
+				}
+			}
+			System.out.println(i);
+			break;
+		}
 	}
 
 }
