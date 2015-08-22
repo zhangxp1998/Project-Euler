a536b55e1d698d65f35d2c1152be941aa047697a
Kelvin Zhang <zhangxp1998@gmail.com>
2015-08-22 11:18:13 -0400

    Add solution to p67

---
+++ src/P67.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class P67
+{
+
+	public static void main(String[] args)
+	{
+		int[][] triangle = new int[100][100];
+		Scanner s = new Scanner(P67.class.getResourceAsStream("res/triangle"));
+		int row = 0;
+		while (s.hasNext())
+		{
+			String str = s.nextLine();
+			String[] strings = str.split(" ");
+			for (int i = 0; i < strings.length; i++)
+			{
+				triangle[row][i] = Integer.parseInt(strings[i]);
+			}
+			row++;
+		}
+		for(int i = 99; i > 0; i --)
+		{
+			for(int j = 0; j < i; j ++)
+			{
+				triangle[i - 1][j] += triangle[i][j] > triangle[i][j + 1] ? triangle[i][j] : triangle[i][j + 1];
+			}
+		}
+		s.close();
+		System.out.println(triangle[0][0]);
+	}
+
+}
